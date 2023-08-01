97fd7068a4ab3592d57b212ee1fc724820a9155d
fleite174 <andretsilva2509@gmail.com>
2023-08-01 10:02:10 -0300

    API LIVROS PRONTO

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ livros = [
     },
     {
         'id': 3,
-        'titulo': 'James Clear',
-        'autor': 'Hábitos Aômicos'
+        'titulo': 'Habitos Atomicos',
+        'autor': 'James Clear'
     },
 ]
 
@@ -25,6 +25,34 @@ livros = [
 def obter_livros():
     return jsonify(livros)
 # Consultar(id)
+@app.route('/livros/<int:id>',methods=['GET'])
+def obter_livro_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
 # Editar
+@app.route('/livros/<int:id>',methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice,livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+# Criar
+@app.route('/livros',methods=['POST'])
+def incluir_novo_livro():
+   novo_livro = request.get_json()
+   livros.append(novo_livro)
+
+   return jsonify(livros)
+
 # Excluir
+@app.route('/livros/<int:id>',methods=['DELETE'])
+def excluir_livros(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+
+    return jsonify
+
 app.run(port=5000,host='localhost',debug=True)
